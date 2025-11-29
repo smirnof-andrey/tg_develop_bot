05312c49b53f2382fa75fc507686340135f0915b
aa.smirnov <aa.smirnov@nexign.com>
2025-11-30 00:18:20 +0300

    init

---
+++ src/main/java/ru/test/tg_develop_bot/TgDevelopBotApplication.java
@@ -0,0 +1,13 @@
+package ru.test.tg_develop_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgDevelopBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgDevelopBotApplication.class, args);
+    }
+
+}
